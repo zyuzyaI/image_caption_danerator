ec201a3821917885c51f6d1167895229d002d604
Kynal Dima <brasskin09@gmail.com>
2020-04-24 17:51:00 +0300

    image caption generator

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# image_caption_danerator
-Image caption generator is a project to recognize the context of an image and describe them in a natural language like English.  The objective of the project is build a working model of Image caption generator by implementing CNN with LSTM.  For the image caption generator, we will be using the Flickr_8K dataset.   [Flicker8k_Dataset](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip) [Flickr_8k_text](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip)  Files in the project.  * Models – contain our trained models. * Descriptions.txt – text file contains all image names and their captions after preprocessing. * Features.p – Pickle object that contains an image and their feature vector extracted from the Xception pre-trained CNN model. * Tokenizer.p – Contains tokens mapped with an index value. * Model.png – Visual representation of dimensions of our project. * Testing_caption_generator.py – Python file for generating a caption of any image. * Training_caption_generator.ipynb – Jupyter notebook in which we train and build our image caption generator.
+Image caption generator is a project to recognize the context of an image and describe them in a natural language like English.
+
+The objective of the project is build a working model of Image caption generator by implementing CNN with LSTM.
+
+For the image caption generator, we will be using the Flickr_8K dataset. 
+
+[Flicker8k_Dataset](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip)
+[Flickr_8k_text](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip)
+
+Files in the project.
+
+* Models – contain our trained models.
+* Descriptions.txt – text file contains all image names and their captions after preprocessing.
+* Features.p – Pickle object that contains an image and their feature vector extracted from the Xception pre-trained CNN model.
+* Tokenizer.p – Contains tokens mapped with an index value.
+* Model.png – Visual representation of dimensions of our project.
+* Testing_caption_generator.py – Python file for generating a caption of any image.
+* Training_caption_generator.ipynb – Jupyter notebook in which we train and build our image caption generator.
---
+++ testing.py
@@ -0,0 +1,70 @@
+"""## Testing the model"""
+import matplotlib.pyplot as plt
+
+import string
+import numpy as np
+from PIL import Image
+import os
+from pickle import dump, load
+import numpy as np
+
+from keras.applications.xception import Xception, preprocess_input
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers.merge import add
+from keras.models import Model, load_model
+from keras.layers import Input, Dense, LSTM, Embedding, Dropout
+
+# small library for seeing the progress of loops.
+from tqdm import tqdm_notebook as tqdm
+
+img_path = "10-Garden-Games-You-Must-Play-With-Your-Kids_1.jpg"
+
+def extract_features(filename, model):
+        try:
+            image = Image.open(filename)
+        except:
+            print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+        image = image.resize((299,299))
+        image = np.array(image)
+        # for images that has 4 channels, we convert them into 3 channels
+        if image.shape[2] == 4: 
+            image = image[..., :3]
+        image = np.expand_dims(image, axis=0)
+        image = image/127.5
+        image = image - 1.0
+        feature = model.predict(image)
+        return feature
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+      if index == integer:
+         return word
+    return None
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'start'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        pred = model.predict([photo,sequence], verbose=0)
+        pred = np.argmax(pred)
+        word = word_for_id(pred, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'end':
+            break
+    return in_text
+
+#path = 'Flicker8k_Dataset/111537222_07e56d5a30.jpg'
+max_length = 32
+tokenizer = load(open("tokenizer.p","rb"))
+model = load_model('models/model_9.h5')
+xception_model = Xception(include_top=False, pooling="avg")
+photo = extract_features(img_path, xception_model)
+img = Image.open(img_path)
+description = generate_desc(model, tokenizer, photo, max_length)
+print("\n\n")
+print(description)
+plt.imshow(img)
